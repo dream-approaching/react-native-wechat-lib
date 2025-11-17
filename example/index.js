110f3044f14a6b69da609be349a5fb9f0600f3f5
zhengjinshou <zhengjinshou@huawei.com>
2025-11-17 16:58:06 +0800

    feat: worklet

--- example/index.js
+++ example/index.js
@@ -3,7 +3,9 @@ import {name as appName} from './app.json';
 // import App from './src/tabView/viewPage';
 // import App from './src/bottom-sheet';
 // import App from './src/bottom-sheet/bad';
-import App from './client-origin';
+// import App from './src/list';
+// import App from './client-origin';
+import App from './src/reanimated-worklet';
 // import App from './App';
 
 LogBox.ignoreAllLogs(true);
